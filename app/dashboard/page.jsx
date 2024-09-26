472eda8a5b75e26cd25362eab3b0a7f05d96b161
juanCamilo2002 <juan2002djfh@gmail.com>
2024-09-26 13:12:20 -0500

    feat: diseño de la vista dashboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,30 @@
+import ActiveUsersAreaChart from "../components/dashboard/charts/ActiveUsersArea"
+import PopularityBarChart from "../components/dashboard/charts/PopularityBarChart"
+import PreferencesPieChart from "../components/dashboard/charts/PreferencesPieChart"
+import VisitsLineChart from "../components/dashboard/charts/VisitLineChart"
 
-const DashboardPage = async () => {
+
+const DashboardPage = () => {
   return (
-    <div className=''>DashboardPage</div>
+    <div>
+      <div className="flex my-8 gap-3">
+        <div className="w-1/3">
+          <PopularityBarChart />
+        </div>
+        <div className="w-2/3">
+          <VisitsLineChart />
+        </div>
+      </div>
+      <div className="flex my-2 gap-3">
+        <div className="w-2/3">
+          <ActiveUsersAreaChart />
+        </div>
+        <div className="w-1/3">
+          <PreferencesPieChart />
+
+        </div>
+      </div>
+    </div>
   )
 }
 
